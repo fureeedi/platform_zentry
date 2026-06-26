c2c0e92d44e80d2b613dcd8a6f64e47bb37cff2a
Hector_M <escaa2714@gmail.com>
2026-06-25 21:08:07 -0500

    chore: Cambios en el landing page de home

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -11,11 +11,8 @@ export const Home = () => {
 
   return (
     <Row>
-      <Col md="3" className="pad">
-        <span className="hipster rounded" />
-      </Col>
       <Col md="9">
-        <h1 className="display-4">¡Bienvenido, Java Hipster!</h1>
+        <h1 className="display-4">¡Bienvenido, a Zentry!</h1>
         <p className="lead">Esta es su página de inicio</p>
         {account?.login ? (
           <div>
@@ -34,51 +31,9 @@ export const Home = () => {
               contraseña=&quot;user&quot;).
             </Alert>
 
-            <Alert variant="warning">
-              ¿Aún no tienes una cuenta?&nbsp;
-              <Link to="/account/register" className="alert-link">
-                Crea una cuenta
-              </Link>
-            </Alert>
+            <Alert variant="warning">¿Aún no tienes una cuenta?&nbsp;</Alert>
           </div>
         )}
-        <p>Si tiene preguntas sobre JHipster:</p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              Página de inicio de JHipster
-            </a>
-          </li>
-          <li>
-            <a href="https://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              JHipster en Stack Overflow
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              JHipster seguimiento de errores
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              Sala de chat pública de JHipster
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              contacto @jhipster en Twitter
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          Si te gusta JHipster, danos una estrella en{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-          !
-        </p>
       </Col>
     </Row>
   );
